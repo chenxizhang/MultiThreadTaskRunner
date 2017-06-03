b5549acd0d97f800fdba308e7e20db278ce598a1
Ares Chen <arechen@microsoft.com>
2017-06-03 10:38:54 +0800

    Add project files.

---
+++ MultiThreadTaskRunner/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadTaskRunner
+{
+    [Cmdlet(VerbsCommon.New, "MultiTaskJob")]
+    public class RunTask : Cmdlet
+    {
+        [Parameter(Position = 0)]
+        public ScriptBlock Block { get; set; }
+
+        [Parameter(Position = 1)]
+        public int ThreadCount { get; set; }
+
+        [Parameter(Position = 2)]
+        public IEnumerable<object> Source { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            Parallel.ForEach(Source, (obj) =>
+            {
+                Block.Invoke(obj);
+            });
+        }
+    }
+}
